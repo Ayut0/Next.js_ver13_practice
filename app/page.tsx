05ff00d0f299113e617e738c6a966058e3807346
Yuto Yama <yuvto.0420@gmail.com>
2023-10-11 20:33:45 -0700

    Add nested route

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-"use client"
+"use client";
 
+import Link from "next/link";
 import User from "./components/User";
 import WithApolloProvider from "./provider/ApolloProvider";
 
@@ -12,6 +13,7 @@ export default function Home() {
       <div>
         <h1>Home</h1>
         <p>GraphQL with Next.js</p>
+        <Link href='/second-page'>Go to the second page</Link>
       </div>
     </main>
   );
---
+++ app/second-page/nested-route/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IndexPage = () => {
+  return (
+    <div>This is nested route</div>
+  )
+}
+
+export default IndexPage
